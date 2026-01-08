e61247609c167a3e74652ea4f407884fab028ac0
Abhishek Malvankar <asm582@nyu.edu>
2026-01-08 17:51:43 -0500

    fix e2e

--- api/v1alpha1/variantautoscaling_types.go
+++ api/v1alpha1/variantautoscaling_types.go
@@ -97,7 +97,7 @@ type OptimizedAlloc struct {
 	Accelerator string `json:"accelerator"`
 
 	// NumReplicas is the number of replicas for the optimized allocation.
-	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Minimum=1
 	NumReplicas int `json:"numReplicas"`
 }
 
